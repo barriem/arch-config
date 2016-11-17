c346070250566a145d3bc78cb96ce69436d23524
Barrie McGuire <barrie@elmacsoftware.co.uk>
2016-11-17 20:10:26 +0000

    tweak i3 config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,11 @@ set -x WORKSPACE /data/dev/hmrc/services
 set -x JAVA_HOME /usr/lib/jvm/java-8-openjdk
 set -x SBT_OPTS "-Xmx2G -Djava.security.egd=file:/dev/./urandom"
 
+alias gs "git status"
+alias gl "git log"
+alias gad "git add -A"
+alias gpom "git push origin master"
+
 fundle plugin 'tuvistavie/fish-ssh-agent'
 
 set -x PATH $PATH /usr/local/bin
